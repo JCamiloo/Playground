5baa4f8769d0a5efad6909ef6020092dc7c0e12e
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-04-11 17:17:19 -0500

    Forms improved

--- Playground/settings.py
+++ Playground/settings.py
@@ -5,8 +5,6 @@ SECRET_KEY = 'lu0v!#pe@&7_^4p2-p5xdk=t3%@1@hy5%m%#zb#3h%o8n%v#6@'
 DEBUG = True
 ALLOWED_HOSTS = []
 
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -49,7 +47,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Playground.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -59,7 +56,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -79,7 +75,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -93,8 +88,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Page
+from .forms import PageForm
 
-# Create your views here.
 class PageListView(ListView):
     model = Page
 
@@ -13,12 +13,12 @@ class PageDetailView(DetailView):
 
 class PageCreate(CreateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
 class PageUpdate(UpdateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
     template_name_suffix = "_update_form"
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + "?ok"
